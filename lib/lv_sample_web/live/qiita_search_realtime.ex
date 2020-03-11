cf79f30f464103437e8667d28dcc982f733a3abe
im <poson_hemp@yahoo.co.jp>
2020-03-12 08:24:25 +0900

    update: separate HTML to template

--- lib/lv_sample_web/live/qiita_search_realtime.ex
+++ lib/lv_sample_web/live/qiita_search_realtime.ex
@@ -2,33 +2,7 @@ defmodule LvSampleWeb.QiitaSearchRealtime do
   use Phoenix.LiveView
 
   def render(assigns) do
-    ~L"""
-      <p>
-      <%= if @message do %><%= @message %><% end %>
-      </p>
-
-      <form phx-submit="submit" phx-change="change">
-        <input type="text" name="query" value="<%= @query %>" placeholder="調べたいキーワードを入力..." <%= if @loading, do: "readonly" %> >
-        <p>Query: <%= @query %></p>
-        <input type="submit" value="search" onclick="blur()" <%= if @loading, do: "readonly" %> >
-      </form>
-
-      <table>
-        <tr>
-          <th>ID</th>
-          <th>タイトル</th>
-          <th>作成日</th>
-        </tr>
-
-        <%= for result <- @results do %>
-        <tr>
-          <td><%= result[ "id" ] %></td>
-          <td><%= result[ "title" ] %></td>
-          <td><%= result[ "created_at" ] %></td>
-        </tr>
-        <% end %>
-      </table>
-    """
+    LvSampleWeb.LiveView.render("submit.html", assigns)
   end
 
   # def mount(_params, _session, socket) do
